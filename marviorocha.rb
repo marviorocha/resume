72f659227d06e36df7b3ef98340b68aa506460c8
marviorocha <marviorocha@marviorocha.com>
2016-05-23 00:51:37 -0300

    Add resume Marvio Rocha

---
+++ marviorocha.rb
@@ -0,0 +1,42 @@
+<ruby>
+
+# Resume portifolio Marvio Rocha written in ruby
+
+name = "Marvio de Siqueira Rocha"
+mail = "marviorocha@marviorocha.com"
+endress = "Governador Roberto Silveira, Macaé-RJ, 238 Centro"
+linkedin = "http://linkedin.com/in/marviorocha"
+phones = "+55 (22) 999225702 / +55 (22) 2772-1808"
+
+#Objective
+
+objective = "Challenging position in web development fulfilling organizational goals"
+
+#Summary
+
+highlights = "Seven years of accumulated talent and professional experience in PHP programming Extensive knowledge of CSS/HTML jQuery Linux and Unix platforms Apache"
+
+#Work Experience
+
+experience = "Institulo Brasil Estados Unidos - Development & Bussiness date [janeiro de 2005 – janeiro de 2007]"
+
+#Language
+
+language = "English"
+
+#education
+
+education = "C E Matias Neto 2009-2012"
+
+#Other curse
+
+curse = "Master Mind - Recognattion for napoleo hill fundation"
+
+#cover letter
+
+print = " Hello my name is #{name} - (#{mail}) has a objective #{objective}.
+ I've with #{highlights} and experience in #{experience}  "
+
+
+
+</ruby>
